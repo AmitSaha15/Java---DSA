1d6b09abaf0129d164d6fb8a0cd09ddb326cbd7a
Amit Saha <asaha.bwn2000@gmail.com>
2023-11-02 00:20:02 +0530

    array input

---
+++ src/arrayAndArrayList/arrayInput.java
@@ -0,0 +1,20 @@
+package arrayAndArrayList;
+
+import java.util.*;
+
+public class arrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+
+        // input using for loop
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // printing the array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
