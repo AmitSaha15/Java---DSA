0122b1be6df357c437c5c1cd87c3d3832b2fc2d4
Amit Saha <asaha.bwn2000@gmail.com>
2024-01-12 01:31:42 +0530

    Check Palindrome

---
+++ src/strings_and_stringBuilder/Palindrome.java
@@ -0,0 +1,24 @@
+package strings_and_stringBuilder;
+
+public class Palindrome {
+
+    static boolean isPalindrome(String str){
+        if (str.length() == 0) {
+            return true;
+        }
+        str = str.toLowerCase();
+        for (int i = 0; i <= str.length()/2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+
+            if (start != end) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "abcdycba";
+        System.out.println(isPalindrome(str));
+    }
+}
