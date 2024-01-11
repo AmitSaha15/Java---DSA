c62505e3a96d3f439d0e7096792ea45865c0c92f
Amit Saha <asaha.bwn2000@gmail.com>
2024-01-11 23:35:19 +0530

    Pretty printing & format specifiers

---
+++ src/strings_and_stringBuilder/PrettyPrinting.java
@@ -0,0 +1,13 @@
+package strings_and_stringBuilder;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float num = 54.3284864f;
+        // System.out.printf("Formatted number is %.2f",num);
+
+        // System.out.printf("PI: %.3f",Math.PI);
+
+        // format specifiers
+        System.out.printf("My name is %s, I am a %s.", "Amit","student");
+    }
+}
