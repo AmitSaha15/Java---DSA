0ef3968696664244ad6eb48bf9baf98196ad57b1
Amit Saha <asaha.bwn2000@gmail.com>
2024-01-26 02:03:13 +0530

    recursion basic

---
+++ src/recursion/ReverseNum.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        reverse(8573);
+        System.out.println(sum);
+    }
+
+    static int sum = 0;
+    static void reverse(int n){
+        if (n == 0) {
+            return;
+        }
+
+        int rem = n%10;
+        sum = (sum*10) + rem;
+        reverse(n/10);
+    }
+}
