459d53dab613a9c3b89e4c7f81a0cf17cad696c1
Amit Saha <asaha.bwn2000@gmail.com>
2024-02-21 20:35:28 +0530

    recursion string questions

--- src/recursion/recursion_string_qsn/SkipString.java
+++ src/recursion/recursion_string_qsn/SkipString.java
@@ -4,6 +4,7 @@ public class SkipString {
     public static void main(String[] args) {
         String remove = "apple";
         System.out.println(skipWord("bcdapplefg", remove));
+        System.out.println(skipAppNotApple("bcappdapplefg", remove));
     }
 
     static String skipWord(String str, String word){
@@ -12,9 +13,21 @@ public class SkipString {
         }
 
         if(str.startsWith(word)){
-            return skipWord(str.substring(5), word);
+            return skipWord(str.substring(word.length()), word);
         } else{
             return str.charAt(0) + skipWord(str.substring(1), word);
+        }
+    }
+
+    static String skipAppNotApple(String str, String word){
+        if(str.isEmpty()){
+            return "";
+        }
+
+        if(str.startsWith("app") && !str.startsWith(word)){
+            return skipAppNotApple(str.substring(3), word);
+        } else{
+            return str.charAt(0) + skipAppNotApple(str.substring(1), word);
         }
     }
 }
