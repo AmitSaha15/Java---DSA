e6ec2d39aecc6a97a6592a9d83f2f77f0cf7be6d
Amit Saha <asaha.bwn2000@gmail.com>
2024-09-19 01:13:20 +0530

    deque example

--- src/StackAndQueue/DequeExample.java
+++ src/StackAndQueue/DequeExample.java
@@ -6,5 +6,19 @@ import java.util.Deque;
 public class DequeExample {
     public static void main(String[] args) {
         Deque<Integer> deque = new ArrayDeque<>();
+
+        deque.add(54);
+        deque.add(11);
+        deque.add(24);
+        deque.add(10);
+        deque.addFirst(222);
+        deque.addLast(999);
+
+        System.out.println(deque.getFirst()); //222
+        System.out.println(deque.getLast()); //999
+        System.out.println(deque.removeFirst()); //222
+        System.out.println(deque.removeLast()); //999
+        System.out.println(deque.getFirst()); //54
+        System.out.println(deque.getLast()); //10
     }
 }
